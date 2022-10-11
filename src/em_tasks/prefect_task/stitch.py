d00197f23fcf70ef8b9f03b089d1c8e643cd7912
Jan Eglinger <jan.eglinger@fmi.ch>
2022-10-11 21:50:52 +0200

    Remove prefect wrapper tasks
    Closes https://github.com/fmi-faim/em-tasks/issues/2.
    
    Also simplify package structure, and fix call to the stitching plugin (compute_overlap).

--- src/em_tasks/prefect_task/stitch.py
+++
@@ -1,9 +0,0 @@
-import prefect
-from prefect import task
-
-from em_tasks.task.stitch import stitch_tiles
-
-
-@task()
-def stitch_tiles_task(input_dir, tileconf_filename, save_path=None, logger=prefect.context.get("logger")):
-    stitch_tiles(input_dir, tileconf_filename, save_path)
